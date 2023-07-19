6513a3a1e510110e560fe2e7f29983651dfca587
shlomo20 <87497439+shlomo20@users.noreply.github.com>
2023-07-18 20:40:49 -0400

    done

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,15 +10,33 @@ import { WrapperButton } from "components/composite/StepComplete/styled"
 const Invalid: NextPage = () => {
   const { t } = useTranslation()
 
+  const handleGoBack = () => {
+    window.history.back()
+  }
+
+  const handleClickContacts =() => {
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/contact-lenses`
+  }
+
+  const handleClickSunglasses = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/sunglasses`
+  }
+
 
   return (
     <ErrorContainer>
       <img className="four-zero-four-img" src="/img/404.svg" alt="404" />
       <div className="four-zero-four-main-text">Whoops, sorry about that!</div>
       <div className="four-zero-four-sub-text">It looks like the page you're looking for no longer exists.</div>
-      <button className="btn-background four-zero-four-btn" onClick={()=>alert('CONTINUE TO HOMEPAGE')}>
-        CONTINUE TO HOMEPAGE
-      </button>
+      <div className="flex flex-wrap gap-2 justify-center">
+        <button className="btn-background four-zero-four-btn uppercase" onClick={handleClickContacts}>
+          Shop contacts
+        </button>
+        <button className="btn-background four-zero-four-btn uppercase" onClick={handleClickSunglasses}>
+          Shop Sunglasses 
+        </button>
+      </div>
+
     </ErrorContainer>
   )
 }
