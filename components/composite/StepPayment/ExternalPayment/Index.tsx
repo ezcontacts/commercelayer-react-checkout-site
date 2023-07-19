4c64fc78892570b03fb7395896d459e212c212bc
manju <manju@ezcontacts.com>
2023-07-19 17:48:43 +0530

    fixed loader issue

--- components/composite/StepPayment/ExternalPayment/Index.tsx
+++ components/composite/StepPayment/ExternalPayment/Index.tsx
@@ -55,9 +55,10 @@ export const ExternalPaymentCard = ({ paymentToken }: any) => {
   }
 
   const handlePlaceOrder = async (event: any) => {
+    clearServerMessage()
+    setIsLoading(true)
     const order = await getOrderFromRef()
     if (order) {
-      setIsLoading(true)
       event.preventDefault()
       const response = await getData(order)
       if (response) {
@@ -85,9 +86,9 @@ export const ExternalPaymentCard = ({ paymentToken }: any) => {
         )
           .then((response) => response.json())
           .then((result) => {
-            setIsLoading(false)
             if (result) {
               window.location.reload()
+              setIsLoading(false)
             }
           })
           .catch((error) => {
@@ -100,6 +101,13 @@ export const ExternalPaymentCard = ({ paymentToken }: any) => {
           })
       }
     }
+  }
+
+  const clearServerMessage = () => {
+    setCardErrorMessage({
+      isSuccess: true,
+      message: "",
+    })
   }
 
   const getData = (order: Order) => {
@@ -193,6 +201,7 @@ export const ExternalPaymentCard = ({ paymentToken }: any) => {
       ...card,
       cardNumber: formattedNumber,
     })
+    clearServerMessage()
 
     const validationResult = validate(formattedInput)
 
@@ -263,6 +272,7 @@ export const ExternalPaymentCard = ({ paymentToken }: any) => {
       ...card,
       [name]: value,
     })
+    clearServerMessage()
     let isValid = true
     if (cvvOnBlurShowError) {
       if (event.target.value) {
@@ -333,6 +343,7 @@ export const ExternalPaymentCard = ({ paymentToken }: any) => {
       ...card,
       [name]: value,
     })
+    clearServerMessage()
 
     let isValid = true
     if (expiredateBlurShowError) {
