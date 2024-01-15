48aa2f93e11dcfe07fe3df4c6d08f0867f87d705
manju <manju@ezcontacts.com>
2024-01-15 11:20:29 +0530

    TR-482 done order id chnagaes

--- pages/Order.tsx
+++ pages/Order.tsx
@@ -25,11 +25,6 @@ const DynamicCheckout: any = dynamic(
 CheckoutSkeleton.displayName = "Skeleton Loader"
 
 const Order: NextPage = () => {
-  var urlString = window?.location?.href
-  var url = new URL(urlString)
-  var queryParams = url?.searchParams
-  //To get the value of the parameter
-  var itemOrderNumber = queryParams?.get("itemOrderNumber")
   const { settings, retryOnError, isLoading } = useSettingsOrInvalid()
   if (isLoading || (!settings && !retryOnError)) return <CheckoutSkeleton />
 
@@ -44,7 +39,7 @@ const Order: NextPage = () => {
     <DynamicCheckoutContainer settings={settings}>
       <DynamicCheckout
         logoUrl={settings.logoUrl}
-        orderNumber={itemOrderNumber}
+        orderNumber={settings.orderNumber}
         companyName={settings.companyName}
         supportEmail={settings.supportEmail}
         supportPhone={settings.supportPhone}
